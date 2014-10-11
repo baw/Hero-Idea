681d55fb32b79ad99a530854fe12d4398c85935e
Brian <iamamthwiz+GitHub@gmail.com>
2014-10-11 10:10:55 -0700

    update idea model

--- app/models/idea.js
+++ app/models/idea.js
@@ -1,7 +1,12 @@
-var mongoose = require("mongoose");
+module.exports = function (app) {
+    var mongoose = app.get("db");
 
-var ideaSchema = mongoose.Schema({
-    title       : String,
-    description : String,
-    created_on  : { type: Date, default: Date.now }
-});
+    var ideaSchema = mongoose.Schema({
+        title       : String,
+        description : String,
+        created_on  : { type: Date, default: Date.now }
+    });
+
+    mongoose.model("idea", ideaSchema);
+};
+
