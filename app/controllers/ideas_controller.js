cc437429322b84f402d80863464bc470c4385d6b
Brian <iamamthwiz+GitHub@gmail.com>
2014-10-11 11:04:45 -0700

    refactor to method

--- app/controllers/ideas_controller.js
+++ app/controllers/ideas_controller.js
@@ -1,10 +1,14 @@
 function index(req, res, app) {
-    var idea = app.get("db").model("idea");
+    var idea = getModel(app);
 
     idea.find({}).sort({x:-1}).limit(20).exec(function (data) {
         res.json(data);
     });
 };
+
+function getModel(app) {
+    return app.get("db").model("idea");
+}
 
 module.exports = {
     "index": index
