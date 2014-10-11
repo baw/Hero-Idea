24ebeaefcce8bd6205ac740accb4eb70a218ff9a
Brian <iamamthwiz+GitHub@gmail.com>
2014-10-11 12:57:34 -0700

    add phonegap

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : process.env.MONGOHQ_URL // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : process.env.MONGOHQ_URL || "mongodb://localhost/test" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
