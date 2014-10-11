2847a4ec434584da337a2d078bc80872198f4173
Brian <iamamthwiz+GitHub@gmail.com>
2014-10-11 08:19:26 -0700

    add callback url base env variable

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: 'your-secret-clientID-here', // your App ID
 		'clientSecret' 	: 'your-client-secret-here', // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+		'callbackURL' 	: process.env.CALLBACK_URL_BASE + '/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
-		'consumerKey' 		: 'your-consumer-key-here',
-		'consumerSecret' 	: 'your-client-secret-here',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+		'consumerKey' 		: process.env.TWITTER_CONSUMER_KEY,
+		'consumerSecret' 	: process.env.TWITTER_CONSUMER_SECRET,
+		'callbackURL' 		: process.env.CALLBACK_URL_BASE + '/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: 'your-secret-clientID-here',
 		'clientSecret' 	: 'your-client-secret-here',
-		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+		'callbackURL' 	: process.env.CALLBACK_URL_BASE + '/auth/google/callback'
 	}
 
 };
