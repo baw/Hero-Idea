6046e618e4142136cb7817086855b1e11abd2c59
Brian <iamamthwiz+GitHub@gmail.com>
2014-10-11 09:15:46 -0700

    change date to created_on

--- app/models/idea.js
+++ app/models/idea.js
@@ -3,5 +3,5 @@ var mongoose = require("mongoose");
 var ideaSchema = mongoose.Schema({
     title       : String,
     description : String,
-    date        : { type: Date, default: Date.now }
+    created_on  : { type: Date, default: Date.now }
 });
