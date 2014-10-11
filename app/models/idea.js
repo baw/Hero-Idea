c1a94c2f54ed88571d40935484acc67ab9f10231
Brian <iamamthwiz+GitHub@gmail.com>
2014-10-11 11:44:46 -0700

    updated idea schema

--- app/models/idea.js
+++ app/models/idea.js
@@ -4,7 +4,19 @@ module.exports = function (app) {
     var ideaSchema = mongoose.Schema({
         title       : String,
         description : String,
-        created_on  : { type: Date, default: Date.now }
+        created_at  : Date,
+        updated_at  : Date
+    });
+
+    ideaSchema.pre("save", function (next) {
+        var now = new Date();
+
+        this.updated_at = now;
+        if (!this.created_at) {
+            this.created_at = now;
+        }
+
+        next();
     });
 
     mongoose.model("idea", ideaSchema);
