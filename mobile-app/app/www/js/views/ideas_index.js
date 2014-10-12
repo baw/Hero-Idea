1ce2e46da1d824f69f76445974923915f2b7d61d
Taylor Smith <jtsmith0107@gmail.com>
2014-10-11 23:09:10 -0700

    upvotes and downvotes

--- mobile-app/app/www/js/views/ideas_index.js
+++ mobile-app/app/www/js/views/ideas_index.js
@@ -1,26 +1,36 @@
 HeroIdea.Views.IndexView = Backbone.View.extend({
+	
 	initialize: function() {
 		this.listenTo(this.collection, 'add', this.render);
+		this.upVoted = false;
+		this.downVoted = false;
+		this.voted = false;
 	},
 	
 	events: {
 		'click .right-slider' : 'ideaShow',
-		'click .panel-group' : 'showDescription'
-
+		'click .panel-group' : 'showDescription',
+		'click .upvote' : 'upvote',
+		'click .downvote' : 'downvote'
 	},
 
   template: _.template($('#idea-index').html()),
+	
+	upvote : function(event){
+		$(event.target).toggleClass('voted');
+		$(event.target).parent().find('.downvote').removeClass('voted');
+	},
+	
+	downvote: function(event){
+		$(event.target).toggleClass('voted');
+		$(event.target).parent().find('.upvote').removeClass('voted');
+		},
 	
 	showDescription: function(event){
 		console.log('fizz');
 		modelID = $(event.target).attr('data-id');
 		// model = HeroIdea.Collections.ideas.getOrFetch(modelID);
 		$('[data-id = "' + modelID + '"]').parent().parent().find('.collapse').collapse('toggle');
-
-		// $(event.target).find('.description').html(model.escape('description'));
-		//
-		// $(event.target).addClass('active-idea');
-
 	},
 	
 	ideaShow: function(event){
